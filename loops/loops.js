77095d99d65fe46eda8c8efc49fb5f112c49db72
Jahnavi <jahnavi@Jahnavis-MacBook-Pro.local>
2026-06-04 12:16:35 +0530

    added loops

--- loops/loops.js
+++ loops/loops.js
@@ -1,61 +1,19 @@
-// for
-let age = 20;
-
-if (age >= 18) {
-  console.log("You can vote!");
+// for 
+for(let i=0;i<5;i++){
+    console.log(i);
 }
-
-let score = 45;
-
-if (score >= 50) {
-  console.log("You passed!");
+for(let i=5;i>0;i--){
+    console.log(i);
 }
-
-
-let isit = true;
-
-if (!isit) {
-  console.log("yaay");
+// while
+let i=0;
+while(i<5){
+    console.log(i);
+    i++;
 }
-
-// else
-let AGE = 15;
-
-if (AGE >= 18) {
-  console.log("You can vote!");
-} else {
-  console.log("Too young");
-}
-
-
-let Score = 72;
-
-if (Score >= 50) {
-  console.log("Passed!");
-} else {
-  console.log("Failed");}
-
-// elseif
-let SCORE = 75;
-
-if (SCORE >= 90) {
-  console.log("Grade: A");
-} else if (SCORE >= 70) {
-  console.log("Grade: B");
-}  else {
-  console.log("Fail");
-}
-
-// switch
-let day = "Monday";
-
-switch (day) {
-  case "Monday":
-    console.log("Start of the week");
-    break;
-  case "Friday":
-    console.log("Weekend is near!");
-    break;
-  default:
-    console.log("No day");
-}
+// do while
+let x=10;
+do{
+    console.log("this runs once");
+    x++;
+}while(x<5);
