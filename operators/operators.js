48680b2ef7cdcec91cdf214c347963fcdb9d544a
Jahnavi <jahnavi@Jahnavis-MacBook-Pro.local>
2026-06-04 10:09:05 +0530

    added operators

---
+++ operators/operators.js
@@ -0,0 +1,34 @@
+// arithmetic
+let a=20, b=30;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a**b);
+console.log(a%b);
+console.log('hello'+'jahnavi');
+
+//assignment
+let score=20;
+score+=10;
+score-=2;
+score*=2;
+score/=4;
+console.log(score);
+let count=0;
+count++;
+count--;
+count++;
+console.log(count);
+
+//comparison
+let age=18;
+console.log(age===18);
+console.log(age===20);
+console.log(age!==14);
+console.log(age>18);
+console.log(age<18);
+console.log(age>=18);
+console.log(age<=18);
+console.log(5==='5');
+console.log(5=='5');
