3268b349c82c0ee615ca1b5b8cca9d7ac36ca445
Jahnavi <jahnavi@Jahnavis-MacBook-Pro.local>
2026-06-12 10:04:06 +0530

    added mini project

---
+++ task 7/digital-clock/script.js
@@ -0,0 +1,23 @@
+function updateClock() {
+    let now = new Date();
+
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+
+    let ampm = hours >= 12 ? "PM" : "AM";
+
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+
+    hours = String(hours).padStart(2, "0");
+    minutes = String(minutes).padStart(2, "0");
+    seconds = String(seconds).padStart(2, "0");
+
+    document.getElementById("clock").textContent =
+        `${hours}:${minutes}:${seconds} ${ampm}`;
+}
+
+updateClock();
+
+setInterval(updateClock, 1000);
